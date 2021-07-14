444fdbcbf63151dc317f61a158dfcd575624f6a4
Luke Menton <lukementon15@gmail.com>
2021-07-14 19:44:37 +0100

    sidebar done, email list in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import EmailList from "./components/email";
 import Header from "./components/header";
+import Mail from "./components/mail";
+import Sidebar from "./components/sidebar";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app_body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route>
+              <EmailList path="/" />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/section/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Section.css";
+
+const Section = () => {
+  return (
+    <div className="section">
+      <h1>heading</h1>
+    </div>
+  );
+};
+
+export default Section;
